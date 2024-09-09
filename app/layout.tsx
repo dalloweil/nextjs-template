ea16cc63452d3f9082b85c52d4b1bc7fed165e83
jwcbmat <johnathan.wcb@gmail.com>
2024-09-08 23:26:40 -0300

    feat: add theme provider, dark variables style and rename global style file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/layout/footer/footer";
-import "./globals.css";
+import "./global.css";
 import dynamic from "next/dynamic";
+import ThemeProvider from "@/components/theme-provider";
 
 const Header = dynamic(() => import("@/components/layout/header/header"), {
   ssr: false,
@@ -21,11 +22,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" suppressHydrationWarning>
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
